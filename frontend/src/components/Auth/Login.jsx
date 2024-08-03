7ff9dce36a34f559c9ad47a04c0ad372322fa5f8
Himanshu Verma <kumarsurajverma6001@gmail.com>
2024-08-03 23:42:55 +0530

    Sign up and Log in DOne

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -1,4 +1,8 @@
-import { Link } from 'react-router-dom';
+import { USER_API } from '@/assets/constant';
+import axios from 'axios';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 import LogIn from '../../assets/Login.svg';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
@@ -6,51 +10,112 @@ import { Label } from '../ui/label';
 import { RadioGroup, RadioGroupItem } from '../ui/radio-group';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    role: 'student',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleRadioChange = (value) => {
+    setFormData({
+      ...formData,
+      role: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${USER_API}/login`, formData, {
+        withCredentials: true,
+      });
+      if (res.data.success) {
+        navigate('/');
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="w-full px-16 py-4">
-      <form action="" className="border border-gray-400 rounded-md p-4 ">
+      <form
+        onSubmit={handleSubmit}
+        className="border border-gray-400 rounded-md p-4"
+      >
         <div className="w-full flex gap-4">
           <div className="w-1/2">
-            <img src={LogIn} alt="" />
+            <img src={LogIn} alt="Login illustration" />
           </div>
           <div className="w-1/2 flex flex-col gap-2 justify-center">
-            <h1 className=" font-bold text-2xl  text-center">Log In</h1>
+            <h1 className="poppins-medium font-bold text-4xl text-center text-[#6A38C2]">
+              Log In
+            </h1>
 
             <div>
               <Label>Email</Label>
-              <Input type="email" placeholder="demo@example.com" className="" />
+              <Input
+                type="email"
+                name="email"
+                placeholder="demo@example.com"
+                value={formData.email}
+                onChange={handleChange}
+                className=""
+              />
             </div>
 
             <div>
               <Label>Password</Label>
               <Input
                 type="password"
+                name="password"
                 placeholder="Enter your password here.."
+                value={formData.password}
+                onChange={handleChange}
                 className=""
               />
             </div>
 
             <div className="mx-auto pt-4">
-              <RadioGroup className="flex items-center gap-8">
+              <RadioGroup
+                value={formData.role}
+                onValueChange={handleRadioChange}
+                className="flex items-center gap-8"
+              >
                 <div className="flex items-center space-x-2">
                   <RadioGroupItem
                     value="student"
                     id="r1"
-                    className=" text-[#6A38C2] border-[#6A38C2]"
+                    checked={formData.role === 'student'}
+                    className="text-[#6A38C2] border-[#6A38C2]"
                   />
                   <Label htmlFor="r1">Student</Label>
                 </div>
                 <div className="flex items-center space-x-2">
                   <RadioGroupItem
-                    value="recruciter"
+                    value="recruiter"
                     id="r2"
-                    className=" text-[#6A38C2] border-[#6A38C2]"
+                    checked={formData.role === 'recruiter'}
+                    className="text-[#6A38C2] border-[#6A38C2]"
                   />
-                  <Label htmlFor="r2">Recruciter</Label>
+                  <Label htmlFor="r2">Recruiter</Label>
                 </div>
               </RadioGroup>
             </div>
-            <Button className="mt-4 bg-[#6A38C2] hover:bg-[#4712a1] text-lg">
+            <Button
+              type="submit"
+              className="mt-4 bg-[#6A38C2] hover:bg-[#4712a1] text-lg"
+            >
               Log in
             </Button>
             <div className="flex justify-center items-center font-medium">
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import { Toaster } from './components/ui/sonner.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <Toaster closeButton />
   </BrowserRouter>
 );
